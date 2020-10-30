fd5195b7be240c82d6165dba17c8194b435233f2
jql <953551525@qq.com>
2020-10-30 19:53:07 +0800

    10-30

---
+++ CASClient2/src/main/java/com/offcn/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.offcn.controller;
+
+/**
+ * @author jinqilin
+ * @version 1.0.0
+ * @PROJECT_NAME CASDemo
+ * @PACKAGE_NAME com.offcn.controller
+ * @ClassName MyController.java
+ * @Description TODO
+ * @createTime 2020年10月30日 19:51:00
+ */
+public class MyController {
+
+    public String test(){
+        return "MyController";
+    }
+}
