a58ac8bb0617563f72470daa501cea46bb75f856
jql <953551525@qq.com>
2020-10-30 19:57:22 +0800

    10-30  分支提交001

---
+++ CASClient2/src/main/java/com/offcn/controller/GitHub.java
@@ -0,0 +1,17 @@
+package com.offcn.controller;
+
+/**
+ * @author jinqilin
+ * @version 1.0.0
+ * @PROJECT_NAME CASDemo
+ * @PACKAGE_NAME com.offcn.controller
+ * @ClassName GitHub.java
+ * @Description TODO
+ * @createTime 2020年10月30日 19:54:00
+ */
+public class  GitHub {
+
+    public String GitHub(){
+        return "HelloHub";
+    }
+}
